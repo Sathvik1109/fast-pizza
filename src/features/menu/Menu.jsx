0317295920175a824d03c1300f39cadf7df69b01
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-04-20 18:27:02 -0500

    feat: fetching data from the router

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
+
+const Menu = () => {
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader = async () => {
+  const menu = await getMenu();
+  return menu;
+};
+
+export default Menu;
