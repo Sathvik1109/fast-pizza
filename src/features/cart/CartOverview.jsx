e28a0191f9ec02fedc0b479f250c9f086bb96f85
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-04-30 16:24:40 -0500

    feat: updated cart quantity and cart price in the cart overview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getCartPrice, getCartQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
+  const cartQuantity = useSelector(getCartQuantity);
+  const cartPrice = useSelector(getCartPrice);
+
+  if (!cartQuantity) return null;
+
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm text-stone-200 uppercase sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span className="">23 pizzas</span>
-        <span>$23.45</span>
+        <span className="">{cartQuantity} pizzas</span>
+        <span>{formatCurrency(cartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
