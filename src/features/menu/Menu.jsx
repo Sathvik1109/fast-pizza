dc7e47c432df80626a18d400fd9764c3460949ce
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-05-03 16:34:29 -0500

    feat: added feature to add priority after placing order

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,7 +4,7 @@ import MenuItem from '../menu/MenuItem';
 
 const Menu = () => {
   const menu = useLoaderData();
-  console.log(menu);
+  // console.log(menu);
 
   return (
     <ul className="divide-y divide-stone-200">
@@ -18,7 +18,7 @@ const Menu = () => {
 // eslint-disable-next-line react-refresh/only-export-components
 export const loader = async () => {
   const menu = await getMenu();
-  console.log(menu);
+  // console.log(menu);
   return menu;
 };
 
